70f9a64225539b7e42d4d0419c9d18025df73c0a
Hiba <hiba.mashfij@gmail.com>
2021-04-21 16:35:49 +0300

    added place class

---
+++ src/crypToursit/assembly/models.ts
@@ -0,0 +1,21 @@
+import { context, u128, PersistentVector } from 'near-sdk-as';
+
+type AccountId = string
+type PlaceId = u32 // index in places Vector
+type PlaceName = string
+
+@nearBindgen
+export class Place {
+  owner: string;
+  constructor(
+    public name: PlaceName,
+    public description: string,
+    public image: string,
+    public price: u128,
+    // public geolocation: Geolocation,
+    public max_radius: u16,
+    public max_owners: u32 = 10,
+    public total_owners: u32 = 10
+  ) {
+    this.owner = context.sender;
+  }
